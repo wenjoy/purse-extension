7829161ab8e06bc95c8ebaf8311134c14858268c
wenjun <laojiaodaoniu@126.com>
2022-08-30 14:45:39 +0800

    chore: determine structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import {resolve} from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'popup.html')
+      }
+    }
+  }
 })
