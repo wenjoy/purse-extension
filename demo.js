7ff005adec30678d4faebd125b20fc29b506d8b2
wenjun <laojiaodaoniu@126.com>
2022-09-12 11:19:03 +0800

    refact: extract logic of generate mnemonic words seed

--- demo.js
+++ demo.js
@@ -1,43 +1,77 @@
 import crypto from 'crypto'
-import {readFile} from 'fs'
+import util from 'node:util'
+import { readFile } from 'node:fs/promises'
 
-const BYTE_LEN = 16
-const ENT = BYTE_LEN * 8 / 32
-const entroph = crypto.randomBytes(BYTE_LEN)
-const entrophBinary = []
-
-for (const item of entroph) {
-  entrophBinary.push(item.toString(2).padStart(8, '0'))
+function generateEntroph(byteLength = 16) {
+  return crypto.randomBytes(byteLength)
 }
 
-const entrophHash = crypto.createHash('sha256').update(entroph).digest()
-const checksum = entrophHash[0].toString(2).padStart(8, '0').slice(0, ENT)
-const entrophWithChecksum = entrophBinary.join('') + checksum
-const mnemonicIndex = []
-let mnemonic = []
+function converToBinary(buffer) {
+  const binaries = []
 
-let temp = entrophWithChecksum
-
-while(temp.length > 0) {
-  mnemonicIndex.push(parseInt(temp.slice(0,11), 2))
-  temp = temp.slice(11)
-}
-console.log('entroph', mnemonicIndex);
-
-readFile('./src/utils/wordlist', (err, data) => {
-  if(err) {
-    console.error(err);
-    return
+  for (const item of buffer) {
+    binaries.push(item.toString(2).padStart(8, '0'))
   }
-  const wordlist = data.toString().split('\n')
-  mnemonic = mnemonicIndex.map((i) => wordlist[i])
+  return binaries
+}
 
-  crypto.pbkdf2(mnemonic.join(' '),'mnemonic',2048, 64, 'sha512', (err, key) => {
-    if(err) {
-      console.error(err);
-    }
-    console.log('wordlist: ', mnemonic.join(' '));
+function sha256(input) {
+  return crypto.createHash('sha256').update(input).digest()
+}
+
+function getENT(entroph) {
+  return entroph.byteLength * 8 / 32
+}
+
+function getChecksum(hash, ENT) {
+  hash[0].toString(2).padStart(8, '0').slice(0, ENT)
+}
+
+function groupForIndex(bits) {
+  let temp = bits
+  const indexes = []
+
+  while (temp.length > 0) {
+    indexes.push(parseInt(temp.slice(0, 11), 2))
+    temp = temp.slice(11)
+  }
+  return indexes
+}
+
+async function generateWords(indexes) {
+  try {
+    let mnemonic = []
+    const data = await readFile('./src/utils/wordlist')
+    const wordlist = data.toString().split('\n')
+    mnemonic = indexes.map((i) => wordlist[i])
+    return mnemonic
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function generateSeed(words) {
+  const pbkdf2 = util.promisify(crypto.pbkdf2)
+  try {
+    const key = await pbkdf2(words.join(' '), 'mnemonic', 2048, 64, 'sha512')
     console.log('key: ', key.toString('hex'));
-    console.log('key length: ', key.byteLength);
-  })
-})
+    return key
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function main() {
+  const entroph = generateEntroph()
+  const entrophBinary = converToBinary(entroph)
+  const entrophHash = sha256(entroph)
+  const ENT = getENT(entroph)
+  const checksum = getChecksum(entrophHash, ENT)
+  const entrophWithChecksum = entrophBinary.join('') + checksum
+  const wordsIndexes = groupForIndex(entrophWithChecksum)
+  const words = await generateWords(wordsIndexes)
+  const seed = await generateSeed(words)
+  console.log('seed', seed);
+}
+
+main()
