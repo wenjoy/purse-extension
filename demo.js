440f4f41cbedfd24c5cd01066d13f5215e8dec8e
wenjun <laojiaodaoniu@126.com>
2022-09-16 15:49:23 +0800

    feat: implement child key derivation

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 import crypto from 'crypto'
 import util from 'node:util'
 import { readFile } from 'node:fs/promises'
+import secp256k1 from 'secp256k1'
 
 function generateEntroph(byteLength = 16) {
   return crypto.randomBytes(byteLength)
@@ -18,6 +19,10 @@ function converToBinary(buffer) {
 function sha256(input) {
   return crypto.createHash('sha256').update(input).digest()
 }
+
+// function sha512(input) {
+//   return crypto.createHash('sha512').update(input).digest()
+// }
 
 function getENT(entroph) {
   return entroph.byteLength * 8 / 32
@@ -54,10 +59,102 @@ async function generateSeed(words) {
   const pbkdf2 = util.promisify(crypto.pbkdf2)
   try {
     const key = await pbkdf2(words.join(' '), 'mnemonic', 2048, 64, 'sha512')
-    console.log('key: ', key.toString('hex'));
+    // console.log('key: ', key.toString('hex'));
     return key
   } catch (err) {
     console.error(err);
+  }
+}
+
+function hmac(input, key) {
+  return crypto.createHmac('sha512', key).update(input).digest('hex')
+}
+
+function buf2Hex(arrayBuffer) {
+  return [...arrayBuffer].map((x) => x.toString(16).padStart(2, '0')).join('')
+}
+
+function derivePubKeyFromPrvKey(prvKey) {
+  const buf = Buffer.from(prvKey, 'hex')
+  return buf2Hex(secp256k1.publicKeyCreate(buf))
+}
+
+function generateMasterkey(seed) {
+  const key = 'Bitcoin seed'
+  const hash = hmac(seed, key)
+  console.log('hash: ', hash);
+
+  const masterPrivateKey = hash.slice(0, 64)
+  console.log('master prv key: ', masterPrivateKey);
+
+  const masterChainCode = hash.slice(64)
+  console.log('mater chain code: ', masterChainCode);
+
+  const masterPublicKey = derivePubKeyFromPrvKey(masterPrivateKey)
+  return [masterPrivateKey, buf2Hex(masterPublicKey), masterChainCode]
+}
+
+function normalChildPrvKeyDerivation(parentPubKeyWithIndex, chainCode, parentPrvKey) {
+  // js Number.MAX_SAFE_INTEGER is 2 ** 53 -1 = 9007199254740991 
+  console.log('normal child prv key======================');
+  const ORDER_OF_CURVE = BigInt('115792089237316195423570985008687907852837564279074904382605163141518161494337')
+  const hash = hmac(parentPubKeyWithIndex, chainCode)
+  const childPrvKey = (BigInt(`0x${parentPrvKey}`) + BigInt(`0x${hash.slice(0, 64)}`)) % ORDER_OF_CURVE
+  console.log('normal child Prv Key: ', childPrvKey.toString(16));
+  console.log('normal child Pub Key: ', derivePubKeyFromPrvKey(childPrvKey.toString(16)));
+
+  const childChainCode = hash.slice(64)
+  console.log('child Chain Code: ', childChainCode);
+  return [childPrvKey, childChainCode]
+}
+
+function normalChildPubKeyDerivation(parentPubKeyWithIndex, chainCode, parentPubKey) {
+  // js Number.MAX_SAFE_INTEGER is 2 ** 53 -1 = 9007199254740991 
+  console.log('normal child pub key======================');
+  const ORDER_OF_CURVE = BigInt('115792089237316195423570985008687907852837564279074904382605163141518161494337')
+  const hash = hmac(parentPubKeyWithIndex, chainCode)
+  const childPubKey = (BigInt(`0x${parentPubKey}`) + BigInt(`0x${hash.slice(0, 64)}`)) % ORDER_OF_CURVE
+  console.log('normal child Pub Key: ', childPubKey.toString(16));
+
+  const childChainCode = hash.slice(64)
+  console.log('child Chain Code: ', childChainCode);
+
+  return [childPubKey, childChainCode]
+}
+
+function hardenedChildPrvKeyDerivation( parentPrvKeyWithIndex, chainCode) {
+  console.log('hardened child prv key======================');
+  const ORDER_OF_CURVE = BigInt('115792089237316195423570985008687907852837564279074904382605163141518161494337')
+  const hash = hmac(parentPrvKeyWithIndex, chainCode)
+  const childPrvKey = (BigInt(`0x${parentPrvKeyWithIndex}`) + BigInt(`0x${hash.slice(0, 64)}`)) % ORDER_OF_CURVE
+  console.log('hardened child Prv Key: ', childPrvKey.toString(16));
+
+  const childChainCode = hash.slice(64)
+  console.log('child Chain Code: ', childChainCode);
+
+  return [childPrvKey, childChainCode]
+}
+
+function childKeyGenerator() {
+  const NORMAL_CHILD_BOUNDARY = 2 ** 16
+  let index = 0
+
+  return (prvKey, pubKey, chainCode) => {
+    let prvKey_
+    let pubKey_
+    let chainCode_
+    const pubKeyWithIndex = pubKey + String(index)
+    const prvKeyWithIndex = prvKey + String(index)
+
+    if(index < NORMAL_CHILD_BOUNDARY) {
+      [prvKey_, chainCode_] = normalChildPrvKeyDerivation(pubKeyWithIndex, chainCode, prvKey)
+      [pubKey_] = normalChildPubKeyDerivation(pubKeyWithIndex, chainCode, pubKey)
+    }else {
+      [prvKey_, chainCode_] = hardenedChildPrvKeyDerivation(prvKeyWithIndex, chainCode)
+    }
+    index++
+
+    return [prvKey_, pubKey_, chainCode_]
   }
 }
 
@@ -71,7 +168,12 @@ async function main() {
   const wordsIndexes = groupForIndex(entrophWithChecksum)
   const words = await generateWords(wordsIndexes)
   const seed = await generateSeed(words)
-  console.log('seed', seed);
+  console.log('seed', seed.toString('hex'))
+  let [prvKey, pubKey, chainCode] = generateMasterkey(seed)
+
+  let generateChildKey = childKeyGenerator();
+
+  [prvKey, pubKey, chainCode] = generateChildKey(prvKey, pubKey, chainCode)
 }
 
 main()
