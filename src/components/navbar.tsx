bd8c4f67ee7f0cf956aad028efa072e576356429
wenjun <laojiaodaoniu@126.com>
2022-11-07 23:08:30 +0800

    fix: style issue

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,13 @@
 interface Props {
-  name: string
+  name: string;
 }
 
 const Navbar = ({ name }: Props) => {
-  return <span className='font-bold underline hover:text-gray-500'>{name}</span>;
+  return (
+    <span className="font-bold underline hover:text-gray-500 cursor-pointer">
+      {name}
+    </span>
+  );
 };
 
 export default Navbar;
